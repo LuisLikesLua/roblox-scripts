3e908b29b2fff9f16a20cc342b7a65b7dce164bb
fluentInLua <genevasuggestion@hotmail.com>
2026-04-09 22:43:01 +0100

    Create ESP.lua

---
+++ ESP.lua
@@ -0,0 +1,52 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "ESP launched",
+	Text = "Find those peeps!",
+	Duration = 3
+})
+
+local thisAtt = Instance.new("Attachment")
+thisAtt.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+
+local atts = {}
+local beams = {}
+local highs = {}
+
+while task.wait(2) do
+	for _,v in pairs(atts) do
+		v:Destroy()
+	end
+  
+	for _,v in pairs(beams) do
+		v:Destroy()
+	end
+
+	for _,v in pairs(highs) do
+		v:Destroy()
+	end
+  
+	for _,v in pairs(game.Players:GetChildren()) do
+		if v == game.Players.LocalPlayer then continue end
+		if not v.Character then continue end
+		if not v.Character:FindFirstChild("HumanoidRootPart") then continue end
+		if v.Character.HumanoidRootPart:FindFirstChild("ESP_ATT") then continue end
+
+		local newHigh = Instance.new("Highlight")
+		newHigh.FillTransparency = 1
+		newHigh.Parent = v.Character
+		table.insert(highs, newHigh)
+    
+		local espAtt = Instance.new("Attachment")
+		espAtt.Name = "ESP_ATT"
+		espAtt.Parent = v.Character.HumanoidRootPart
+		table.insert(atts, espAtt)
+    
+		local espBeam = Instance.new("Beam")
+		espBeam.Parent = v.Character
+		espBeam.Width0 = 0.2
+		espBeam.Width1 = 0.2
+		espBeam.Attachment0 = thisAtt
+		espBeam.Attachment1 = espAtt
+		espBeam.FaceCamera = true
+		table.insert(beams, espBeam)
+	end
+end
