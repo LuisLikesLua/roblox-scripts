b79f8c22b20177a8b254ef94ccb396eb78e44af1
fluentInLua <genevasuggestion@hotmail.com>
2026-04-10 17:30:04 +0100

    Update Decaying_winter_ESP.lua
    fix hopedully

--- Decaying_winter_ESP.lua
+++ Decaying_winter_ESP.lua
@@ -11,15 +11,15 @@ while task.wait(2) do
 		v:Destroy()
 	end
   
-	for _,v in pairs(workspace:GetDescendants()) do
-		if not v:IsA("Humanoid") then continue end
+	for _,v in pairs(workspace.activeHostiles:GetChildren()) do
+		if not v:FindFirstChild("Humanoid") then continue end
 		if game.Players:GetPlayerFromCharacter(v.Parent) then continue end
 
 		local gui = Instance.new("BillboardGui")
 		gui.Size = UDim2.new(4, 0, 6, 0)
 		gui.AlwaysOnTop = true
 		gui.LightInfluence = 0
-		gui.Parent = v.Parent
+		gui.Parent = v
 		table.insert(guis, gui)
 		
 		local frame = Instance.new("Frame")
@@ -29,6 +29,7 @@ while task.wait(2) do
 		task.spawn(function()
 			local healthFrame = Instance.new("Frame")
 			healthFrame.Parent = gui
+			healthFrame.BorderSizePixel = 0
 			healthFrame.BackgroundTransparency = 0.5
 			healthFrame.BackgroundColor3 = Color3.new(1,0,0)
 			
@@ -36,8 +37,8 @@ while task.wait(2) do
 				if not healthFrame then return end
 				if not v.Parent then continue end
 				
-				healthFrame.Size = UDim2.new(1,0,v.Health/v.MaxHealth,0)
-				healthFrame.Position = UDim2.new(0,0,1-(v.Health/v.MaxHealth),0)
+				healthFrame.Size = UDim2.new(1,0,v.Humanoid.Health/v.Humanoid.MaxHealth,0)
+				healthFrame.Position = UDim2.new(0,0,1-(v.Humanoid.Health/v.Humanoid.MaxHealth),0)
 			end
 		end)
 		
